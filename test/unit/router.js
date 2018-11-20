0422eaeaf17de7aed43b712bf3218cf930e830d6
liuwill <liuwei_will@qq.com>
2018-11-20 10:20:15 +0800

    base router support middleware

--- test/unit/router.js
+++ test/unit/router.js
@@ -4,12 +4,30 @@ const RouterRegister = require('../../lib/routes/register')
 const { expect, assert } = chai
 
 describe('RouterRegister test', function () {
-  it('would RouterRegister be build', function () {
+  it('should RouterRegister be build', function () {
     const sourceRouter = new Router()
     const secondRouter = new Router()
 
     const register = new RouterRegister(sourceRouter)
     expect(register.router()).to.be.equal(sourceRouter)
     expect(register.router()).to.be.not.equal(secondRouter)
+  })
+
+  it('should RouterRegister register middleware', function () {
+    const sourceRouter = new Router()
+    const middleRouter = new Router()
+    const register = new RouterRegister(sourceRouter)
+    const getFunc = async () => { }
+    const middleware = async () => { }
+    middleRouter.get('user', getFunc)
+
+    register.register('middle', middleRouter, middleware)
+    register.register('group', middleRouter, [middleware])
+
+    const matchRouter = sourceRouter.match('middle/user')
+    const groupRouter = sourceRouter.match('group/user')
+
+    expect(middleware).to.be.equal(matchRouter.path[0].stack[0])
+    expect(middleware).to.be.equal(groupRouter.path[0].stack[0])
   })
 })
